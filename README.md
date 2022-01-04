306365f331064f760b4ac41853e8c06f9357ed7b
Pierre-Portfolio <60719810+Pierre-Portfolio@users.noreply.github.com>
2022-01-04 19:16:20 +0100

    Add All for Cast

---
+++ README.md
@@ -0,0 +1,33 @@
+<h1 align="center">
+  <img src="./static/Velo-Gif.gif" alt="Bikes" />
+</h1>
+
+---
+
+# ESILV - Seoul Bike Sharing
+## About Dataset
+
+- Currently Rental bikes are introduced in many urban cities for the enhancement of mobility comfort. It is important to make the rental bike available and accessible to the public at the right time as it lessens the waiting time. Eventually, providing the city with a stable supply of rental bikes becomes a major concern.
+
+- The dataset contains weather information (Temperature, Humidity, Windspeed, Visibility, Dewpoint, Solar radiation, Snowfall, Rainfall), the number of bikes rented per hour and date information.
+
+## Problem
+
+The crucial part is the prediction of bike count required at each hour for the stable supply of rental bikes
+
+## Tech Stack
+PYTHON
+JUPYTER
+FLASK
+
+## Api Demo
+![App Screenshot](./static/demoCompressed.mp4)
+
+## Conclusion
+This projects was a real challenge for us as well on a technical level with software like Django or Github
+
+
+## Authors
+
+- [@Pierre](https://github.com/Pierre-Portfolio)
+- [@Anthony](https://github.com/Cyd-des-Tenebres)
---
+++ livrable/api/serveur/main.py
@@ -0,0 +1,71 @@
+#import librairies
+print("Loading Librairies ...")
+import pandas as pd
+import numpy as np
+from math import *
+
+from xgboost import XGBRegressor
+from flask import Flask , render_template, request
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+
+#Charge the Dataset
+print("Loading Dataset ...")
+SeoulBikeDf = pd.read_csv("./static/csv/SeoulBikeData.csv", encoding="latin1")
+
+#Transformation quantitative variable to vector
+SeoulBikeDf['Functioning Day'] = SeoulBikeDf['Functioning Day'].replace(to_replace=['No', 'Yes'], value=[0, 1])
+SeoulBikeDf['Holiday'] = SeoulBikeDf['Holiday'].replace(to_replace=['No Holiday', 'Holiday'], value=[0, 1])
+SeoulBikeDf['Seasons'] = SeoulBikeDf['Seasons'].replace(to_replace=['Winter', 'Spring', 'Summer', 'Autumn'], value=[0, 1, 2, 3])
+
+#We transfrom Date data into 4 new columns :  day, month, years and dayofweek
+SeoulBikeDf['Years'] = pd.to_datetime(SeoulBikeDf['Date']).dt.year
+SeoulBikeDf['Month'] = pd.to_datetime(SeoulBikeDf['Date']).dt.month
+SeoulBikeDf['Day'] = pd.to_datetime(SeoulBikeDf['Date']).dt.day
+
+#Delete columns
+SeoulBikeDf = SeoulBikeDf.drop(['Date', 'Dew point temperature(°C)', 'Years'], axis = 1)
+
+#Target & features
+x = SeoulBikeDf.drop(['Rented Bike Count'], axis = 1)
+y = SeoulBikeDf["Rented Bike Count"]
+
+#Create train & test set
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33)
+    
+#Scale
+scaler = StandardScaler()
+scaler.fit(x_train)       
+x_train = scaler.transform(x_train, copy = False)
+x_test  = scaler.transform(x_test, copy = False)
+
+#params
+algo = XGBRegressor()
+hyperparametres = {
+    "max_depth" : [4],
+    "gamma" : [0.077, 0.5, 0.75, 1]
+}
+
+#Fitting Model         
+grid = GridSearchCV(algo, hyperparametres, n_jobs=-1)
+grid.fit(x_train, y_train)
+
+app = Flask(__name__)
+print("Loading : Done")
+
+#Launch the main page
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+#Launch on the button event
+@app.route('/', methods=['POST'])
+def homepredict():
+    predictDf = [[int(request.form['Hour']), float(request.form['Temperature']), int(request.form['Humidity']), float(request.form['WindSpeed']), float(request.form['Visibility']), float(request.form['SolarRadiation']), float(request.form['Rainfall']), float(request.form['Snowfall']), int(request.form['Season']), int(request.form['Holiday']), int(request.form['FunctioningDay']), int(request.form['Month']), int(request.form['Day'])]]
+    print(predictDf)
+    
+    result = grid.predict(predictDf)
+    return render_template('index.html', prediction = "The prediction for this features : " + str(ceil(result[0])))
+
+app.run(host='127.0.0.1', port=8080, debug=False)

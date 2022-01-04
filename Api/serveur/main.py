65c2406a007c27a156f81a61f9937fd67d7ad49f
Pierre-Portfolio <60719810+Pierre-Portfolio@users.noreply.github.com>
2022-01-04 02:04:16 +0100

    start back end API

--- Api/serveur/main.py
+++ Api/serveur/main.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, request
 
 app = Flask(__name__)
 
@@ -6,8 +6,8 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route("/trollAsterion/<ddd>/<ccc>")
-def TROL(ddd, ccc):
-    return render_template('index.html', asterion = "GG")
+@app.route('/', methods=['POST'])
+def TROL():
+    return render_template('index.html', prediction = request.form['Hour'])
 
 app.run(host='127.0.0.1', port=8080, debug=True)
